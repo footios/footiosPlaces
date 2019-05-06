8eaf076cc93b87acb3c273c561eb51f9c4580f12
Fotis Tsakiris <footios76@gmail.com>
2019-05-06 12:04:29 +0300

    Custom button added

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -6,6 +6,7 @@ import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
 import MainText from '../../components/UI/MainText/MainText';
 import backgroundImage from '../../assets/background.jpg';
+import ButtondWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground' 
 
 /* Screens are components we load through RNNavigation (instead of Router)! */
 
@@ -21,13 +22,13 @@ class AuthScreen extends Component {
 					<MainText>
 						<HeadingText>Please Log in</HeadingText>
 					</MainText>
-					<Button title="Switch to Login" />
+					<ButtondWithBackground color='#29aaf4' onPress={() => alert('hello world')} >Switch to Login</ButtondWithBackground>
 					<View style={styles.inputContainer}>
 						<DefaultInput placeholder="Your e-mail adress" style={styles.input} />
 						<DefaultInput placeholder="Password" style={styles.input} />
 						<DefaultInput placeholder="Confirm password" style={styles.input} />
 					</View>
-					<Button title="Submit" onPress={this.loginHandler} />
+					<ButtondWithBackground color='#29aaf4' onPress={this.loginHandler}>Submit</ButtondWithBackground>
 				</View>
 			</ImageBackground>
 		);
