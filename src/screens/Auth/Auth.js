398a4357793b94d8f547c3832e1beddaf4318014
Fotis Tsakiris <footios76@gmail.com>
2019-05-07 09:57:02 +0300

    sidedrawer asignment

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
+
+import HeadingText from '../../components/UI/HeadingText/HeadingText'
 
 class SideDrawer extends Component {
 	render() {
 		return (
 			<View style={[styles.container, {width: Dimensions.get('window').width * 0.8}]} >
-				<Text>SideDrawer</Text>
+				<View style={styles.title} >
+				<View style={styles.sideDraweIcon}  >
+					<Icon size={30} name='ios-list-box' color='black' />
+				</View>
+					<HeadingText>SideDrawer</HeadingText>
+				</View>
+				<View style={styles.logout} >
+					<Text style={{fontSize: 20}} >Log out</Text>
+				<TouchableOpacity onPress={() => alert('You are logged out')} >
+				<View style={styles.logoutIcon}  >
+					<Icon size={30} name='ios-log-out' color='black' />
+				</View>
+				</TouchableOpacity>
+				</View>
 			</View>
 		);
 	}
@@ -16,8 +32,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 22,
-        backgroundColor: 'white',
-    }
+		backgroundColor: 'white',
+		justifyContent: 'space-between',
+	},
+	title: {
+		margin: 25,
+		flexDirection: 'row'
+	},
+	sideDraweIcon: {
+		margin: 3,
+		marginRight: 5,
+	},
+	logout: {
+		marginBottom: 355,
+		marginLeft: 30,
+		flexDirection: 'row',
+		alignItems: 'center',
+		fontSize: 30
+	},
+	logoutIcon: {
+		margin: 3,
+		marginLeft: 15,
+	}
 })
 
 export default SideDrawer;
