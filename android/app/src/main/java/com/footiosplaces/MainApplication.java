a95c9242403c1ca3ffe2f26d5def7473b5e024ef
Fotis Tsakiris <footios76@gmail.com>
2019-05-15 12:44:59 +0300

    image picker installed...

--- android/app/src/main/java/com/footiosplaces/MainApplication.java
+++ android/app/src/main/java/com/footiosplaces/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -57,9 +58,9 @@ public class MainApplication extends NavigationApplication {
 		// Add additional packages you require here
 		// No need to add RnnPackage and MainReactPackage
 		return Arrays.<ReactPackage>asList(
-      // eg. new VectorIconsPackage()
       new VectorIconsPackage(),
-      new MapsPackage()
+      new MapsPackage(),
+      new ImagePickerPackage()
 		);
 	}
 
